9921b0713940c30829093fd1c809c433e417c290
Owen Dearman <owendearman@gmail.com>
2022-05-11 21:09:27 +0100

    added header and footer information

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,3 +1,30 @@
 export function Footer(): JSX.Element {
-  return <div></div>;
+  return (
+    <>
+      <h3>Thank you for playing Battordle!</h3>
+      <h3>
+        This App was created by Owen Dearman (GitHub: @owen-dearman). The
+        repository can be found{" "}
+        <a
+          href="https://github.com/owen-dearman/wordle-2-player"
+          rel="noreferrer"
+          target="_blank"
+        >
+          here
+        </a>{" "}
+      </h3>
+      <h3>
+        This app is associated in no way to{" "}
+        <a
+          href="https://www.nytimes.com/games/wordle/index.html"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Wordle
+        </a>{" "}
+        and any resemblance between this app and the aforementioned property is
+        coincidental.
+      </h3>
+    </>
+  );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,8 @@
 export function Header(): JSX.Element {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Battordle</h1>
+      <h2>The Real-Time Word Guessing Mastermind Game for 2 Players</h2>
+    </div>
+  );
 }
