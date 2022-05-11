053a4365e60785838ed929e5a7d6e5682cf24b5e
Owen Dearman <owendearman@gmail.com>
2022-05-11 21:56:34 +0100

    imported logo image, added favicon, css formatting for footer and title

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer(): JSX.Element {
   return (
-    <>
-      <h3>Thank you for playing Battordle!</h3>
-      <h3>
+    <footer>
+      <p>Thank you for playing Battordle!</p>
+      <p>
         This App was created by Owen Dearman (GitHub: @owen-dearman). The
         repository can be found{" "}
         <a
@@ -11,9 +11,9 @@ export function Footer(): JSX.Element {
           target="_blank"
         >
           here
-        </a>{" "}
-      </h3>
-      <h3>
+        </a>.
+      </p>
+      <p>
         This app is associated in no way to{" "}
         <a
           href="https://www.nytimes.com/games/wordle/index.html"
@@ -24,7 +24,7 @@ export function Footer(): JSX.Element {
         </a>{" "}
         and any resemblance between this app and the aforementioned property is
         coincidental.
-      </h3>
-    </>
+      </p>
+    </footer>
   );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,12 @@
+
 export function Header(): JSX.Element {
   return (
-    <div>
-      <h1>Battordle</h1>
-      <h2>The Real-Time Word Guessing Mastermind Game for 2 Players</h2>
-    </div>
+    <header>
+      <div className="headerContainer">
+      <img className="logo" src="battordle_logo.png" alt="The Battordle logo is a cartoon battle axe with the word battordle in yellow and green along the hilt" />
+      <h1 className="pageTitle">BATTORDLE!</h1>
+      </div>
+      <h2 className="subtitle">The Real-Time Word Guessing Mastermind Game for 2 Players</h2>
+    </header>
   );
 }
