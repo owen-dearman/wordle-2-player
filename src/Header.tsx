4705bf824bdcc7ebdb241c1f73bc9dfb30bc74b1
Owen Dearman <owendearman@gmail.com>
2022-05-12 12:50:40 +0100

    updated header CSS

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,9 +4,11 @@ export function Header(): JSX.Element {
     <header>
       <div className="headerContainer">
       <img className="logo" src="battordle_logo.png" alt="The Battordle logo is a cartoon battle axe with the word battordle in yellow and green along the hilt" />
-      <h1 className="pageTitle">BATTORDLE!</h1>
+      <div className="headerSubContainer">
+        <h1 className="pageTitle">B A T T O R D L E !</h1>
+        <h2 className="subtitle">The Real-Time Word Guessing Mastermind Game for 2 Players</h2>
       </div>
-      <h2 className="subtitle">The Real-Time Word Guessing Mastermind Game for 2 Players</h2>
+      </div>
     </header>
   );
 }
